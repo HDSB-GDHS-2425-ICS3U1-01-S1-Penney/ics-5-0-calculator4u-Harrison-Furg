6bbac1db06224ea42b8e6bc687ca9573d648c895
Harrison-Furg <1fergusonhar@hdsb.ca>
2024-09-16 12:47:18 -0400

    added error checking

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-let equation
-function operate(){
-    equation = prompt("Welcome to the A.S.M.D Calculator! Type your equation here or type Rules for the rules.");
+//storing equation
+let equation;
+//storing error function
+let error = false;
+
+//operating function
+function operate() {
+    error = false;
+    equation = prompt("Welcome to the A.S.M.D Calculator! Type your equation here or type 'Rules' for the rules.");
+
+    //check if user wants rules page then displays rules
     if (equation == "Rules") {
-        prompt("Only +, -, *, or / are accepted. Numbers only, words crash the program. Press enter to start calculator.")
+        prompt("Only +, -, *, or / are accepted. Numbers only, words crash the program. To do exponents write them with two * Ex: 3**2 = 9. Press enter to start calculator.")
         operate();
     } else {
-        if (isNaN(eval(equation))){
-            prompt("Error, is not a number. Press enter to restart.")
+        //Checks if equation doenst have letters
+        if (errorCode(equation) == true) {
+
+            if (eval(equation) == Infinity) {
+                prompt("Error, can not devide by zero. Press enter to restart.")
+                operate();
+            }
+            else {
+                prompt("Your answer is " + eval(equation) + ". Press enter to restart calculator.");
+                operate();
+            }
+        } else {
+            prompt("Error, can not contain letters. Press enter to restart.")
             operate();
-        } else if (eval(equation) == Infinity) {
-            prompt("Error, can not devide by zero. Press enter to restart.") 
-            operate();
-        }else {
-        prompt("Your answer is " + eval(equation) + ". Press enter to restart calculator.");
-        operate();}
+        }
+
     }
 }
+
+//checks wheather string contains letters
+function errorCode(str) {
+    const letterPattern = /[a-zA-Z]/;
+    return !letterPattern.test(str);
+}
+
+//call operating function
 operate()
